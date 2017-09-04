c5aac30c0d8f8df2fd6bd6ac0ac70a76f0f75f61
folo <folochina@gmail.com>
2017-09-04 17:23:33 +0800

    update

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 /data/ydmap/node/ydmap-ssr-portal/stop.sh
 sleep 5
 /data/ydmap/node/ydmap-ssr-portal/start.sh
-sleep 20
-tail -f /data/ydmap/node/ydmap-ssr-portal/ssr.log
+# sleep 20
+# tail -f /data/ydmap/node/ydmap-ssr-portal/ssr.log
