6c0304343cd7c0d6dbe3d90713f7f513b7211529
yoyo837 <yoyo837@hotmail.com>
2017-09-05 16:29:53 +0800

    extensions

--- backpack.config.js
+++ backpack.config.js
@@ -1,6 +1,10 @@
+const _ = require('lodash')
+
 module.exports = {
   webpack: (config, options, webpack) => {
     config.entry.main = './server/index.js'
+
+    config.resolve.extensions = _.union(config.resolve.extensions, ['.js', '.vue', '.json'])
     return config
   }
 }
