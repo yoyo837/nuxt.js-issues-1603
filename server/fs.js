392022b2cbccb760c75af1e825ec80f9ac0b79c4
yoyo837 <yoyo837@hotmail.com>
2017-09-09 16:24:54 +0800

    fs 改写成异步

--- server/fs.js
+++ server/fs.js
@@ -6,62 +6,63 @@ export default {
    * 创建目录，如果不存在
    * @param {string} dir
    */
-  mkdir(dir) {
+  async mkdir(dir) {
     if (dir == null) {
       return
     }
-    fsextra.ensureDirSync(dir)
+    await fsextra.ensureDir(dir)
   },
   /**
    * 复制文件
    * @param {string} src
    * @param {string} dest
    */
-  copy(src, dest) {
+  async copy(src, dest) {
     if (src == null || dest == null) {
       return
     }
     console.log(`copy from ${src} to ${dest}`)
-    fsextra.copySync(src, dest)
+    await fsextra.copy(src, dest)
   },
   /**
    * 清空目录/删除目录下所有
    * @param {string} dir
    */
-  emptyDir(dir) {
+  async emptyDir(dir) {
     if (dir == null) {
       return
     }
-    this.removeFileFromDir(dir, file => {
-      return file.toLowerCase() !== '.gitkeep'
-    })
+    await this.removeFileFromDir(dir)
   },
   /**
    * 删除目录下部分文件/目录
    * @param {string} dir
    * @param {function} fn
    */
-  removeFileFromDir(dir, fn) {
+  async removeFileFromDir(dir, fn) {
     if (dir == null) {
       return
     }
     fn = typeof fn === 'function' ? fn : file => {
       return true
     }
-    fs.readdirSync(dir).forEach(file => {
-      if (fn(file)) {
-        this.removeFile(`${dir}/${file}`)
-      }
+    await new Promise((resolve, reject) => {
+      fs.readdirSync(dir).forEach(async file => {
+        if (fn(file)) {
+          await this.removeFile(`${dir}/${file}`)
+        }
+      })
+      resolve()
     })
   },
   /**
    * 删除文件/目录
    * @param {string} file
    */
-  removeFile(file) {
+  async removeFile(file) {
     if (file) {
       console.log('del', file)
-      fsextra.removeSync(file)
+      await fsextra.remove(file)
     }
   }
 }
--- server/index.js
+++ server/index.js
@@ -21,91 +21,92 @@ config.dev = !(app.env === 'production')
 // fsextra.removeSync(/^\.nuxt/) // fsextra.removeSync 不支持正则
 svfs.removeFileFromDir('.', file => {
   return file.toLowerCase().startsWith('.nuxt')
-})
-
-// 是否正在构建
-const Separator = '-'
-// let isBuilding = false
-let buildIndex = 0
-let nuxt
-// Instantiate nuxt.js
-function nuxtBuild() {
-  // isBuilding = true
-  if (nuxt) {
-    config.build.buildDir = `${nuxt.options.buildDir.split(Separator)[0]}-${++buildIndex}` // 换一个目录
-  }
-  const innerNuxt = new Nuxt(config) // 如果重复利用Nuxt, nuxt在build的时候是不能提供服务的, 所以每次new
-  if (nuxt == null) { // 初始化的时候第一次没有，直接赋值
-    nuxt = innerNuxt
-  }
-
-  const pagePath = path.join(innerNuxt.options.rootDir, 'pages') // nuxt的pages页面目录
-  svfs.emptyDir(pagePath) // 清空
-  svfs.copy(config.skin.getPagesPath.apply(innerNuxt), pagePath) // 复制新的
-  // svfs.copy('pagestest', 'pages') // 复制新的
-
-  const builder = new Builder(innerNuxt)
-  builder.build().then(() => {
-    if (nuxt === innerNuxt) {
-    } else {
-      const temp = nuxt
-      nuxt = innerNuxt
-      temp.close()
+}).then(() => {
+  // 是否正在构建
+  const Separator = '-'
+  // let isBuilding = false
+  let buildIndex = 0
+  let nuxt
+  // Instantiate nuxt.js
+  function nuxtBuild() {
+    // isBuilding = true
+    if (nuxt) {
+      config.build.buildDir = `${nuxt.options.buildDir.split(Separator)[0]}-${++buildIndex}` // 换一个目录
     }
-    // isBuilding = false
-    console.log('Build completed!')
-  }).catch(e => {
-    console.error(e)
-    process.exit(1)
-  })
-}
+    const innerNuxt = new Nuxt(config) // 如果重复利用Nuxt, nuxt在build的时候是不能提供服务的, 所以每次new
+    if (nuxt == null) { // 初始化的时候第一次没有，直接赋值
+      nuxt = innerNuxt
+    }
 
-// Build in development
-const doAPIReg = /\.do$/
-const cp = c2k(proxy({
-  target: 'http://localhost:8082/portal/'
-}))
-
-if (config.dev) {
-  router.get(doAPIReg, cp)
-
-  router.post(doAPIReg, cp)
-
-  nuxtBuild()
-}
-
-app.use(bodyParser())
-
-app.use(router.routes())
-app.use(router.allowedMethods({
-  throw: true,
-  /* eslint-disable new-cap */
-  notImplemented: () => new Boom.notImplemented(),
-  methodNotAllowed: () => new Boom.methodNotAllowed()
-}))
-
-router.post('/sync', (ctx, next) => {
-  console.log('收到同步请求 ---> ', ctx.request.body)
-  ctx.status = 200
-  ctx.response.body = {
-    result: 123
+    const pagePath = path.join(innerNuxt.options.rootDir, 'pages') // nuxt的pages页面目录
+    // 清空旧的复制新的
+    svfs.emptyDir(pagePath).then(svfs.copy(config.skin.getPagesPath.apply(innerNuxt), pagePath)).then(() => {
+      const builder = new Builder(innerNuxt)
+      builder.build().then(() => {
+        if (nuxt === innerNuxt) {
+        } else {
+          const temp = nuxt
+          nuxt = innerNuxt
+          temp.close()
+        }
+        // isBuilding = false
+        console.log('Build completed!')
+      }).catch(e => {
+        console.error(e)
+        process.exit(1)
+      })
+    }).catch(e => {
+      console.log(e)
+    })
   }
 
-  nuxtBuild.apply(this)
-})
+  // Build in development
+  const doAPIReg = /\.do$/
+  const cp = c2k(proxy({
+    target: 'http://localhost:8082/portal/'
+  }))
 
-router.get(/(^\/_nuxt(?:\/|$))|(^\/(?:__webpack_hmr|$)$)/, async function(ctx, next) {
-  ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+  if (config.dev) {
+    router.get(doAPIReg, cp)
 
-  await new Promise((resolve, reject) => {
-    ctx.res.on('close', resolve)
-    ctx.res.on('finish', resolve)
-    nuxt.render(ctx.req, ctx.res, promise => {
-      // nuxt.render passes a rejected promise into callback on error.
-      promise.then(resolve).catch(reject)
+    router.post(doAPIReg, cp)
+
+    nuxtBuild()
+  }
+
+  app.use(bodyParser())
+
+  app.use(router.routes())
+  app.use(router.allowedMethods({
+    throw: true,
+    /* eslint-disable new-cap */
+    notImplemented: () => new Boom.notImplemented(),
+    methodNotAllowed: () => new Boom.methodNotAllowed()
+  }))
+
+  router.post('/sync', (ctx, next) => {
+    console.log('收到同步请求 ---> ', ctx.request.body)
+    ctx.status = 200
+    ctx.response.body = {
+      result: 123
+    }
+
+    nuxtBuild.apply(this)
+  })
+
+  router.get(/(^\/_nuxt(?:\/|$))|(^\/(?:__webpack_hmr|$)$)/, async function(ctx, next) {
+    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+
+    await new Promise((resolve, reject) => {
+      ctx.res.on('close', resolve)
+      ctx.res.on('finish', resolve)
+      nuxt.render(ctx.req, ctx.res, promise => {
+        // nuxt.render passes a rejected promise into callback on error.
+        promise.then(resolve).catch(reject)
+      })
     })
   })
-})
 
-app.listen(port, host)
-console.log(`Server listening on ${host}:${port}`)
+  app.listen(port, host)
+  console.log(`Server listening on ${host}:${port}`)
+})
