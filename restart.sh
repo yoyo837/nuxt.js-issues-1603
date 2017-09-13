223300dabf54b33d62b46a04ebdd1a40b83e7c86
yoyo837 <yoyo837@hotmail.com>
2017-09-13 15:41:52 +0800

    备份node_modules

--- restart.sh
+++ restart.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 /data/ydmap/services/jenkins/test/workspace/ydmap-ssr-portal/stop.sh
+
 sleep 5
+
+rm -rf /data/ydmap/node/node_modules/
+
+cp -r /data/ydmap/node/ydmap-ssr-portal/node_modules/ /data/ydmap/node/
 
 rm -rf /data/ydmap/node/ydmap-ssr-portal
 
 mkdir -p /data/ydmap/node/ydmap-ssr-portal
 
 cp -r /data/ydmap/services/jenkins/test/workspace/ydmap-ssr-portal/ /data/ydmap/node/
+
+cp -r /data/ydmap/node/node_modules/ /data/ydmap/node/ydmap-ssr-portal/
 
 BUILD_ID=DONTKILLME
 
