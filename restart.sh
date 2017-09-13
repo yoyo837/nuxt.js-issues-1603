c0cd0bebb2e90d036b4eb541bfa0eba81ec77bce
yoyo837 <yoyo837@hotmail.com>
2017-09-13 15:39:05 +0800

    指令挪到restart

--- restart.sh
+++ restart.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-/data/ydmap/node/ydmap-ssr-portal/stop.sh
+/data/ydmap/services/jenkins/test/workspace/ydmap-ssr-portal/stop.sh
 sleep 5
+
+rm -rf /data/ydmap/node/ydmap-ssr-portal
+
+mkdir -p /data/ydmap/node/ydmap-ssr-portal
+
+cp -r /data/ydmap/services/jenkins/test/workspace/ydmap-ssr-portal/ /data/ydmap/node/
+
+BUILD_ID=DONTKILLME
+
 /data/ydmap/node/ydmap-ssr-portal/start.sh
 # sleep 20
 # tail -f /data/ydmap/node/ydmap-ssr-portal/ssr.log
