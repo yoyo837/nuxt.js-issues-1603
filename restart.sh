a24a308f3fec4774f1802745e069971ab8206c63
yoyo837 <yoyo837@hotmail.com>
2017-09-14 11:58:08 +0800

    echo 由于jenkins似乎会一定几率杀死start.sh拉起的ssr-portal应用的进程，出发点尚不明确。故此，需要手动启动此应用以跳过此问题后续解决。

--- restart.sh
+++ restart.sh
@@ -17,6 +17,7 @@ cp -r /data/ydmap/node/node_modules/ /data/ydmap/node/ydmap-ssr-portal/
 
 cd /data/ydmap/node/ydmap-ssr-portal/
 
+echo 由于jenkins似乎会一定几率杀死start.sh拉起的ssr-portal应用的进程，出发点尚不明确。故此，需要手动启动此应用以跳过此问题后续解决。
 # ./start.sh
 
 # sleep 20
