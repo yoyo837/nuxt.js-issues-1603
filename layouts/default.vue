3685dd148d2706c07c33e4f3df4183ce5b361751
yoyo837 <yoyo837@hotmail.com>
2017-09-15 18:51:29 +0800

    pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,7 +28,7 @@ export default {
 @import '~@/assets/css/module/skin-app.scss';
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue","Avenir",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
