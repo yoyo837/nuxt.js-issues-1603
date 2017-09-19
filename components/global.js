049492e007e423e84273d2421bda4dfcb5db7c7d
yoyo837 <yoyo837@hotmail.com>
2017-09-19 14:58:51 +0800

    完善太对

--- components/global.js
+++ components/global.js
@@ -6,6 +6,7 @@ Vue.config.productionTip = false
 
 Vue.prototype.$http = ajax
 Vue.prototype.CDN_STATIC_HOST = utils.CDN_STATIC_HOST
+Vue.prototype.CDN_IMG_HOST = utils.CDN_IMG_HOST
 
 export default function (router) {
   ajax.setRouter(router)
