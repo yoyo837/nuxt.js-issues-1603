5ac0d8a94cbc90edda80bb891fd7e2d7dbe16e6d
yoyo837 <yoyo837@hotmail.com>
2017-09-20 18:01:25 +0800

    $wxConfig

--- components/global.js
+++ components/global.js
@@ -8,13 +8,21 @@ Vue.prototype.$http = ajax
 Vue.prototype.CDN_STATIC_HOST = utils.CDN_STATIC_HOST
 Vue.prototype.CDN_IMG_HOST = utils.CDN_IMG_HOST
 
-Vue.prototype.$wxConfig = async function(url) {
+Vue.prototype.$wxConfig = async function (url, throwError) {
+  if (typeof url === 'boolean') {
+    throwError = url
+    url = null
+  }
   if (utils.isWeiXin()) {
     await this.$http.get('/pubUser/getJsConfig.do', {
       url: url || (utils.isiOS ? utils.entryUrl : (process.browser ? window.location.href.split('#')[0] : ''))
     })
+  } else {
+    if (throwError) {
+      throw new Error('请在微信中运行该功能')
+    }
+    await {}
   }
-  await {}
 }
 
 export default function (router) {
