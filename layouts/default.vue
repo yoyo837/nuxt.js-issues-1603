c68eddc6a449ae92ad3bbb23b752802c8fd26aca
yoyo837 <yoyo837@hotmail.com>
2017-10-10 18:54:36 +0800

    wechat-limit

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,16 +9,48 @@
 </template>
 
 <script>
-import SkinApp from '../pages/vue-features/components/SkinApp'
+import _ from 'lodash'
+import utils from '../components/utils'
 import global from '../components/global'
+import SkinApp from '../pages/vue-features/components/SkinApp'
+
+const limitPath = '/error/wechat-limit'
 
 export default {
   name: 'app',
   beforeMount() {
+    // this.$router.beforeResolve((to, from, next) => {
+    //   this.checkAndRouteWeChatLimit(to, from, next)
+    // })
     global(this.$router)
+    // this.checkAndRouteWeChatLimit()
   },
   components: {
     SkinApp
+  },
+  methods: {
+    checkAndRouteWeChatLimit(to, from, next) {
+      to = to || this.$route
+      if (!(to.path === limitPath || utils.isWeiXin())) {
+        // const serviceInfo = this.$webStore.session.get(this.$webStoreKey.srvInfo)
+        const serviceInfo = {
+          onlyInWechat: true
+        }
+        if (serviceInfo && serviceInfo.onlyInWechat) {
+          if (_.isFunction(next)) {
+            console.log('456', this.$route.path)
+            next(limitPath)
+          } else {
+            console.log('123', this.$route.path)
+            this.$router.push(limitPath)
+          }
+          return
+        }
+      }
+      if (_.isFunction(next)) {
+        next()
+      }
+    }
   }
 }
 </script>
@@ -28,7 +60,7 @@ export default {
 @import '~@/assets/css/module/skin-app.scss';
 
 #app {
-  font-family: "Helvetica Neue","Avenir",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  font-family: "Helvetica Neue", "Avenir", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
