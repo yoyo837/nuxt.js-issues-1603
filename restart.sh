38e51dbdac4df65e28b89b49722d6a4dfc80a04e
唐辉 <yoyo837@hotmail.com>
2017-10-11 14:06:54 +0800

    更新 restart.sh

--- restart.sh
+++ restart.sh
@@ -11,8 +11,9 @@ cp -r /data/ydmap/services/jenkins/test/workspace/ydmap-ssr-portal/ /data/ydmap/
 
 cd /data/ydmap/node/ydmap-ssr-portal/
 
-echo 由于jenkins似乎会一定几率杀死start.sh拉起的ssr-portal应用的进程，出发点尚不明确。故此，需要手动启动此应用以跳过此问题后续解决。
+echo 由于jenkins似乎会一定几率杀死start.sh拉起的ssr-portal应用的进程，触发点尚不明确。故此，需要手动启动此应用以跳过此问题后续解决。
 # ./start.sh
+echo 请在 /data/ydmap/node/ 下执行 ./start.sh
 
 # sleep 20
 # tail -f /data/ydmap/node/ydmap-ssr-portal/ssr.log
